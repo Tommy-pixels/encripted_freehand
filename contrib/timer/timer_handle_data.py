c03b0f72b9d38945e5557fa4396d98eb045fdc73
Tommy <39845906+Tommy-pixels@users.noreply.github.com>
2022-06-10 15:52:11 +0800

    20220610

--- contrib/timer/timer_handle_data.py
+++ contrib/timer/timer_handle_data.py
@@ -6,16 +6,17 @@ class TaskTimer_AutoDealwithPost(Base_Timer):
     def task(self):
         datapool = self.timerConfig["datapool"]
         dotted_path = 'freehand.taskslib.tasks_postdata.' + datapool
+        by_classification = self.timerConfig.get("by_classification") if self.timerConfig.get("by_classification")==False else True
         if (self.timerConfig["task_type"] == 'keyParagraph'):
             key_p = import_string(dotted_path + '.task_post_keyparagraph')
             t = key_p.Task_Post_Keyparagraph()
             t.env_config = self.timerConfig
-            t.run(table_name='tb_key_paragraph', classification=datapool)
+            t.run(table_name='tb_key_paragraph', classification=datapool, by_classification=by_classification)
         elif (self.timerConfig["task_type"] == 'relativeParagraph'):
             rela_p = import_string(dotted_path + '.task_post_relateparagraph')
             t_rela = rela_p.Task_Post_Relativeparagraph()
             t_rela.env_config = self.timerConfig
-            t_rela.run(table_name='tb_relative_paragraph', classification=datapool)
+            t_rela.run(table_name='tb_relative_paragraph', classification=datapool, by_classification=by_classification)
         elif (self.timerConfig["task_type"] == 'contentImgs'):
             contentimg_p = import_string(dotted_path + '.task_post_contentimg')
             contentimg_p.run(proj_absPath=self.timerConfig["proj_absPath"], origin=self.timerConfig["origin"],database=self.timerConfig['databaseName'],
@@ -28,17 +29,17 @@ class TaskTimer_AutoDealwithPost(Base_Timer):
             commotn_p = import_string(dotted_path + '.task_post_comment')
             t_comment = commotn_p.Task_Post_Comment()
             t_comment.env_config = self.timerConfig
-            t_comment.run(table_name='tb_comment', classification=datapool)
+            t_comment.run(table_name='tb_comment', classification=datapool, by_classification=by_classification)
         elif (self.timerConfig['task_type'] == 'articles'):
             article_p = import_string(dotted_path + '.task_post_articles')
             t_articles = article_p.Task_Post_Article()
             t_articles.env_config = self.timerConfig
-            t_articles.run(table_name='tb_article', classification=datapool)
+            t_articles.run(table_name='tb_article', classification=datapool, by_classification=by_classification)
         elif(self.timerConfig['task_type'] == 'questions'):
             question_p = import_module(dotted_path + '.task_post_question')
             t_articles = question_p.Task_Post_Question()
             t_articles.env_config = self.timerConfig
-            t_articles.run(table_name='tb_question', classification=datapool)
+            t_articles.run(table_name='tb_question', classification=datapool, by_classification=by_classification)
         else:
             print('参数 task_type 出错')
 
