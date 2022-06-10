6a725cb5e0c272268d9a078de82e87d41de41125
Tommy <39845906+Tommy-pixels@users.noreply.github.com>
2022-06-10 15:56:40 +0800

    20220610

--- taskslib/tasks_postdata/old_datapool/task_post_relateparagraph.py
+++ taskslib/tasks_postdata/old_datapool/task_post_relateparagraph.py
@@ -15,7 +15,7 @@ class Task_Post_Relativeparagraph(Base_Task_Post):
         Base_Task_Post.__init__(self)
         self.keyword_list = ['个股', '股市', 'A股', '港股', '新股', '美股', '创业板', '证券股', '股票', '炒股', '散户', '短线', '操盘', '波段']
 
-    def run(self, table_name, classification):
+    def run(self, table_name, classification, **kwargs):
         # 1 获取对应数据
         db = DB_Singleton_DEFAULT()
         db.cursor.execute("SELECT id, account,password,api_uri FROM `tb_datapool_info` WHERE `classification`='{}';".format(classification))
